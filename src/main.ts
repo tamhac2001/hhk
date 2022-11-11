2e85f698f49ba3b7f401fa8712e4c035e88ec72c
Ho Hoang Hung <tamhac2001@gmail.com>
2022-11-11 08:43:55 +0700

    enable CORS & add vercel.json

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {});
   app.setGlobalPrefix('/v1/api');
+  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       // skipMissingProperties: true,
